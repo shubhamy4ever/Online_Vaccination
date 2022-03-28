2ed233319b51d15172f46d07fbc0e75e292f8ed0
Shubham Yadav <63555820+shubhamy4ever@users.noreply.github.com>
2022-03-28 22:46:48 +0530

    Updated Versioning

--- src/components/Home.js
+++ src/components/Home.js
@@ -212,7 +212,7 @@ export const Home = () => {
           Copyright &copy; All rights reserved
         </div>
         <div className="centerfooter"> Website By Shubham Y</div>
-        <div className="toright">v2.4.5</div>
+        <div className="toright">v2.4.6</div>
       </footer>
     </div>
   );
